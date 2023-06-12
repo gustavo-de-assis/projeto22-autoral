087c943b4147e782e043f57384db52f4d7733560
gustavo de assis <gustavo.siqueira.421@gmail.com>
2023-06-12 13:46:08 -0300

    feat: signIn and signUp features on backend

---
+++ projeto22-autoral-backend/src/repositories/user-repository/index.ts
@@ -0,0 +1,26 @@
+import { prisma } from "@/config";
+import { CreateUserParams } from "@/protocols";
+
+async function findUserByEmail(email: string) {
+  return prisma.users.findFirst({
+    where: {
+      email,
+    },
+  });
+}
+
+async function createUser(newUser: CreateUserParams) {
+  return prisma.users.create({
+    data: {
+      email: newUser.email,
+      password: newUser.password,
+    },
+  });
+}
+
+const userRepository = {
+  findUserByEmail,
+  createUser,
+};
+
+export default userRepository;
