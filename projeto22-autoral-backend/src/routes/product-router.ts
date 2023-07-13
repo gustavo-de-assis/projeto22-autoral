39307563e5269e53a0f08fca2673add58977fd88
Briofita09 <felipe.osorio.santos@hotmail.com>
2023-07-13 15:15:45 -0300

    Code review

--- projeto22-autoral-backend/src/routes/product-router.ts
+++
@@ -1,8 +0,0 @@
-import { getProduct, getProducts } from "@/controllers/product-controller";
-import { Router } from "express";
-
-const productRouter = Router();
-
-productRouter.get("/", getProducts).get("/:productId", getProduct);
-
-export { productRouter };
